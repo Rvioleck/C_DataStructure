70a8fb2a89b2128a0e4b3a0fad384b5cde30da4f
Rvioleck <352453347@qq.com>
2021-01-21 00:50:39 +0800

    2021年1月21日00:50:36

--- C_Linear_List/LinkListWithHeadWorks.cpp
+++ C_Linear_List/LinkListWithHeadWorks.cpp
@@ -20,26 +20,26 @@ LinkList deleteValueOfX_p38q2(LinkList &L, int x){
 
 void reverseShowList_p38q3(LinkList L){
     /// 利用递归栈来逆置打印 T(n) = O(n), S(n) = O(n);
-    if (L->next){
-        reverseShowList_p38q3(L->next);
-    }
-    if (L) printf("%d->", L->data);
+//    if (L->next){
+//        reverseShowList_p38q3(L->next);
+//    }
+//    if (L) printf("%d->", L->data);
 
     /// 前插法建立新的倒序链表，后输出 T(n) = O(n), S(n) = O(n);
-//    if (!L->next){
-//        return;
-//    }
-//    LinkList rL;
-//    InitList(rL);
-//    LNode *cur = L->next;
-//    while (cur){
-//        LNode *s = (LNode*)malloc(sizeof(LNode));
-//        s->data = cur->data;
-//        s->next = rL->next;
-//        rL->next = s;
-//        cur = cur->next;
-//    }
-//    ShowList(rL);
+    if (!L->next){
+        return;
+    }
+    LinkList rL;
+    InitList(rL);
+    LNode *cur = L->next;
+    while (cur){
+        LNode *s = (LNode*)malloc(sizeof(LNode));
+        s->data = cur->data;
+        s->next = rL->next;
+        rL->next = s;
+        cur = cur->next;
+    }
+    ShowList(rL);
 }
 
 LinkList deleteTheMinElem_p38q4(LinkList &L){
@@ -170,20 +170,142 @@ void showAscendingLinkList_p39q9(LinkList &head){
     printf("NULL\n");
 }
 
-void
+void separateOddAndEvenIndex_p39q10(LinkList L, LinkList &even, LinkList &odd){
+    if (!L->next)
+        return;
+    int flag = 1;
+    LNode *odd0 = odd, *even0 = even;
+    while (L->next){
+        L = L->next;
+        if (flag){
+            odd0->next = L;
+            odd0 = odd0->next;
+            flag = 0;
+        }else{
+            even0->next = L;
+            even0 = even0->next;
+            flag = 1;
+        }
+    }
+    if (flag){
+        odd0->next = NULL;
+    }else{
+        even0->next = NULL;
+    }
+}
+
+LinkList separateOddAndEvenIndex_p39q11(LinkList &A){
+    if (!A->next)
+        return NULL;
+    int flag = 1;
+    LinkList B;
+    InitList(B);
+    LNode *a = A, *b = B; // 作为A和B的头结点
+    LNode *cur = A->next;
+    while (cur){
+        if (flag){
+            flag = 0;
+            a->next = cur;
+            a = a->next;
+            cur = cur->next;
+        }else{
+            flag = 1;
+            b->next = cur;
+            b = b->next;
+            cur = cur->next;
+        }
+    }
+    a->next = NULL;
+    b->next = NULL;
+    return B;
+}
+
+LinkList deleteRepeatedElem_p39q12(LinkList &L){
+    if (!L->next || !L->next->next)
+        return L;
+    LNode *cur = L->next;
+    while (cur->next){
+        LNode *q = cur->next;
+        if (q->data == cur->data){
+            cur->next = q->next;
+            free(q);
+        }else{
+            cur = cur->next;
+        }
+    }
+    return L;
+}
+
+LinkList mergeIntoDescending_p39q13(LinkList L1, LinkList L2){
+    LNode *a = L1->next, *b = L2->next;
+    L1->next = NULL;
+    while (a && b){
+        if (a->data <= b->data){
+            LNode *next = a->next;
+            a->next = L1->next;
+            L1->next = a;
+            a = next;
+        }
+        else{
+            LNode *next = b->next;
+            b->next = L1->next;
+            L1->next = b;
+            b = next;
+        }
+    }
+    if (b)
+        a = b;
+    while (a){
+        LNode *next = a->next;
+        a->next = L1->next;
+        L1->next = a;
+        a = next;
+    }
+    free(a);
+    return L1;
+}
 
 int main() {
     LinkList L;
     InitList(L);
     ListInsert(L, 1, 1);
-    ListInsert(L, 2, 8);
-    ListInsert(L, 3, 3);
-    ListInsert(L, 4, 1);
-    ListInsert(L, 5, 0);
-    ListInsert(L, 6, 5);
+    ListInsert(L, 2, 3);
+    ListInsert(L, 3, 4);
+    ListInsert(L, 4, 5);
+    ListInsert(L, 5, 7);
+    ListInsert(L, 6, 8);
     ShowList(L);
 
+    /// mergeIntoDescending 调试
+//    LinkList L2;
+//    InitList(L2);
+//    ListInsert(L2, 1, 2);
+//    ListInsert(L2, 2, 6);
+//    ListInsert(L2, 3, 9);
+//    ListInsert(L2, 4, 10);
+//    ListInsert(L2, 5, 11);
+//    ListInsert(L2, 6, 12);
+//    ShowList(L2);
+//    L = mergeIntoDescending_p39q13(L, L2);
+//    ShowList(L);
 
+
+    /// deleteRepeatedElem 调试
+//    deleteRepeatedElem_p39q12(L);
+//    ShowList(L);
+
+    /// separateOddAndEvenIndex(LinkList &A) 单参数 调试
+//    LinkList L2 = separateOddAndEvenIndex_p39q11(L);
+//    ShowList(L);
+//    ShowList(L2);
+
+    /// separateOddAndEvenIndex 调试
+//    LinkList even, odd;
+//    InitList(even);
+//    InitList(odd);
+//    separateOddAndEvenIndex_p39q10(L, even, odd);
+//    ShowList(even);
+//    ShowList(odd);
 
     /// showAscendingLinkList 调试
 //    showAscendingLinkList_p39q9(L);
